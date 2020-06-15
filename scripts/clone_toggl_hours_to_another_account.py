3dc6c6d02df8475fa2e4b519bcf1a0069c7228c5
Ville Laitila <ville.laitila@softagram.com>
2020-06-15 17:36:02 +0300

    Setting up two API tokens correctly...

--- scripts/clone_toggl_hours_to_another_account.py
+++ scripts/clone_toggl_hours_to_another_account.py
@@ -1,6 +1,6 @@
 
 # Howto:
-#  TOGGL_APIKEY=b58ded522fbe3c7545b3694565fdf122 python3 scripts/clone_toggl_hours_to_another_account.py 2021-05-01 2021-05-31 920770 9680195 163968195 1204988 442482
+#  TOGGL_APIKEY_FROM=deadbeef TOGGL_APIKEY_TO=cafebabe python3 scripts/clone_toggl_hours_to_another_account.py 2021-05-01 2021-05-31 920770 9680195 163968195 1204988 442482
 # See sys.argv refs for arg documentation
 
 import urllib
@@ -12,7 +12,7 @@ user_agent = 'cloner'
 def get_entries(start, end, from_workspace_id, from_user_id):
     toggl = Toggl()
 
-    toggl.setAPIKey(os.getenv('TOGGL_APIKEY'))
+    toggl.setAPIKey(os.getenv('TOGGL_APIKEY_FROM'))
     response = toggl.request(f'https://toggl.com/reports/api/v2/details?workspace_id={from_workspace_id}&since={start}&until={end}&user_agent={user_agent}&uid={from_user_id}&include_time_entry_ids=true&user_ids={from_user_id}')
     if response['total_count'] > response['per_page']:
         raise Exception('Paging not supported, results exceed the page')
@@ -31,7 +31,7 @@ def get_entries(start, end, from_workspace_id, from_user_id):
 def save_entries(entries, workspace_id, user_id, project_id, hourdiff_to_utc):
     toggl = Toggl()
 
-    toggl.setAPIKey(os.getenv('TOGGL_APIKEY'))
+    toggl.setAPIKey(os.getenv('TOGGL_APIKEY_TO'))
     for entry in entries:
         start = entry['start']
         start_date = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S%z')
